34396c2777d5a25d8c90c54e36c39533aae208fd
Lucero Cruz <lucero154@users.noreply.github.com>
2026-01-15 16:38:15 -0500

    Estructura inicial de componentes del frontend

---
+++ src/app/components/catalog/catalog.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CategoryService, Category } from '../../services/category.service';
+import { ProductService, Product } from '../../services/product';
+
+@Component({
+  selector: 'app-catalog',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './catalog.html',
+  styleUrl: './catalog.css'
+})
+export class Catalog implements OnInit {
+
+  categories: Category[] = [];
+  products: Product[] = [];
+  selectedCategoryId!: number;
+
+  constructor(
+    private categoryService: CategoryService,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
+    });
+  }
+
+  onCategoryChange(): void {
+    this.productService
+      .getProductsByCategory(this.selectedCategoryId)
+      .subscribe(data => this.products = data);
+  }
+}
